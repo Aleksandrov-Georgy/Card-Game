4133f9d80d04d604ad3fbe3bfdc4ca306d327383
Aleksandrov-Georgy <g.aleksandrov116@yandex.ru>
2023-03-12 18:11:04 +0300

    add new file index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+    localStorage.removeItem('level');
+
+    const formLevel = document.querySelector('.header__form');
+    const buttonLevel = document.querySelectorAll('.button');
+
+
+    buttonLevel.forEach(element => {
+        element.addEventListener('click', (event) => {
+            event.stopPropagation()
+            localStorage.setItem('level', element.value);
+        })
+        
+    });
+
+    formLevel.addEventListener('click', (event) => {
+        console.log('Уровень: ' + localStorage.getItem('level'));
+        if(localStorage.getItem('level')) {
+            window.location.href = '2.html';
+        } else {
+            buttonLevel.forEach(element => {
+                element.classList.add('error')
+            });
+            console.log('error');
+        }
+
+    })
+
+
+    
+    
+
+
+})
